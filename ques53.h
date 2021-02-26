0e6e3c4ed4ab61d797c42cb71471615b3add0a5b
chenzhike110 <53006764+chenzhike110@users.noreply.github.com>
2021-02-26 21:51:52 +0800

    Update ques53.h
    4ms，12.8MB

--- ques53.h
+++ ques53.h
@@ -1,23 +1,16 @@
-#include<vector>
-#include<iostream>
-using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         if(nums.size()==0){
             return 0;
         }
-        vector<int> ends;
+        int result = nums[0];
         int temp = nums[0];
         for(int i=1;i<nums.size();i++){
             temp = temp + nums[i];
             if(temp < nums[i]) temp = nums[i];
-            ends.push_back(temp);
+            if(temp > result) result = temp;
         }
-        temp = nums[0];
-        for(int i=0;i<ends.size();i++){
-            if(temp < ends[i]) temp = ends[i];
-        }
-        return temp;
+        return result;
     }
 };
